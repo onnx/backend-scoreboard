a768d295470182b746e146e3c1abd209e0c14b7b
Akshay Chalana <akshayc@fb.com>
2018-08-28 15:46:37 -0700

    Fixed path in write_metadata

--- data/write_metadata.py
+++ data/write_metadata.py
@@ -2,7 +2,7 @@ import csv
 import torch
 import onnx
 
-with open('data/metadata.csv', 'a') as f:
+with open('metadata.csv', 'a') as f:
     writer = csv.writer(f)
     writer.writerow(["ONNX", str(onnx.version.version)])
     writer.writerow(["PyTorch/Caffe2", torch.version.__version__])
