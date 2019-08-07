0ff34e677f83b4201c1206cabd89362e164388aa
mitruska <katarzyna.mitrus@intel.com>
2019-08-07 16:46:54 +0200

    Marked functions as hidden

--- test/conftest.py
+++ test/conftest.py
@@ -31,15 +31,15 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config):
     results_dir = os.environ.get("RESULTS_DIR", os.getcwd())
 
     # Collect and save the results
-    report = prepare_report(terminalreporter.stats)
-    save_report(report, results_dir)
-    summary = prepare_summary(report)
-    trend = load_trend(results_dir)
-    current_trend = update_trend(summary, trend)
-    save_trend(current_trend, results_dir)
+    report = _prepare_report(terminalreporter.stats)
+    _save_report(report, results_dir)
+    summary = _prepare_summary(report)
+    trend = _load_trend(results_dir)
+    current_trend = _update_trend(summary, trend)
+    _save_trend(current_trend, results_dir)
 
 
-def prepare_report(stats):
+def _prepare_report(stats):
     # Return results report based on pytest stats values
     # that match keys listed in report_keys
     report = {"date": datetime.now().strftime("%m/%d/%Y %H:%M:%S")}
@@ -58,7 +58,7 @@ def prepare_report(stats):
     return report
 
 
-def prepare_summary(report):
+def _prepare_summary(report):
     # Return tests summary including number of failed and passed tests
     summary = dict()
     for key in report.keys():
@@ -68,19 +68,19 @@ def prepare_summary(report):
     return summary
 
 
-def save_report(report, results_dir, file_name="report.json"):
+def _save_report(report, results_dir, file_name="report.json"):
     # Save report to the file
     with open(os.path.join(results_dir, file_name), "w") as report_file:
         json.dump(report, report_file, sort_keys=True)
 
 
-def save_trend(trend, results_dir, file_name="trend.json"):
+def _save_trend(trend, results_dir, file_name="trend.json"):
     # Save trend data to the file
     with open(os.path.join(results_dir, file_name), "w") as trend_file:
         json.dump(trend, trend_file, sort_keys=True)
 
 
-def load_trend(results_dir, file_name="trend.json"):
+def _load_trend(results_dir, file_name="trend.json"):
     # Return list of summaries loaded from tests trend file
     # If file is broken, empty or not found create new trend list
     try:
@@ -91,7 +91,7 @@ def load_trend(results_dir, file_name="trend.json"):
     return trend
 
 
-def update_trend(summary, trend):
+def _update_trend(summary, trend):
     # Return updated trend
     # Append result summary if trend has less than two results or
     # the last one result is different than current,
