771c50ee48c999d992938a57673615083d28c4ea
mitruska <katarzyna.mitrus@intel.com>
2019-08-07 16:34:17 +0200

    Black auto formatting

--- test/conftest.py
+++ test/conftest.py
@@ -7,49 +7,52 @@ from datetime import datetime
 
 
 def pytest_addoption(parser):
-    parser.addoption('--onnx_backend',
-                     choices=['ngraph_onnx.onnx_importer.backend',
-                              'onnxruntime.backend.backend',
-                              'onnx_tf.backend',
-                              'caffe2.python.onnx.backend'],
-                     help='Select from available backends')
+    parser.addoption(
+        "--onnx_backend",
+        choices=[
+            "ngraph_onnx.onnx_importer.backend",
+            "onnxruntime.backend.backend",
+            "onnx_tf.backend",
+            "caffe2.python.onnx.backend",
+        ],
+        help="Select from available backends",
+    )
 
 
 def pytest_configure(config):
-    onnx_backend_module = config.getvalue('onnx_backend')
+    onnx_backend_module = config.getvalue("onnx_backend")
     test.ONNX_BACKEND_MODULE = onnx_backend_module
 
 
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
     # Keys for values to save in report (matched with terminalreporter.stats)
-    report_keys = ['passed', 'failed', 'skipped']
+    report_keys = ["passed", "failed", "skipped"]
     # Set directory for results
-    results_dir = os.environ.get('RESULTS_DIR', os.getcwd())
+    results_dir = os.environ.get("RESULTS_DIR", os.getcwd())
 
     # Collect and save the results
     report = prepare_report(terminalreporter.stats, report_keys)
     save_report(report, results_dir)
     summary = prepare_summary(report)
     trend = load_trend(results_dir)
-    actual_trend = update_trend(summary, trend)
-    save_trend(actual_trend, results_dir)
+    current_trend = update_trend(summary, trend)
+    save_trend(current_trend, results_dir)
 
 
 def prepare_report(stats, report_keys):
     # Return results report based on pytest stats values
     # that match keys listed in report_keys
-    report = dict()
-    report['date'] = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+    report = {"date": datetime.now().strftime("%m/%d/%Y %H:%M:%S")}
     for key in report_keys:
         stats_group = stats.get(key, [])
         if isinstance(stats_group, list):
             report[key] = []
             for record in stats_group:
-                if hasattr(record, 'nodeid'):
+                if hasattr(record, "nodeid"):
                     # Remove file name from test id
-                    split_id = record.nodeid.split('::')
-                    clear_id = filter(lambda x: '.py' not in x, split_id)
-                    record_name = '::'.join(clear_id)
+                    split_id = record.nodeid.split("::")
+                    clear_id = filter(lambda x: ".py" not in x, split_id)
+                    record_name = "::".join(clear_id)
                     report[key].append(record_name)
             report[key].sort()
     return report
@@ -61,28 +64,27 @@ def prepare_summary(report):
     for key in report.keys():
         if isinstance(report.get(key), list):
             summary[key] = len(report.get(key))
-    summary['date'] = report.get('date',
-                                 datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
+    summary["date"] = report.get("date", datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
     return summary
 
 
-def save_report(report, results_dir, file_name='report.json'):
+def save_report(report, results_dir, file_name="report.json"):
     # Save report to the file
-    with open(os.path.join(results_dir, file_name), 'w') as report_file:
+    with open(os.path.join(results_dir, file_name), "w") as report_file:
         json.dump(report, report_file, sort_keys=True)
 
 
-def save_trend(trend, results_dir, file_name='trend.json'):
+def save_trend(trend, results_dir, file_name="trend.json"):
     # Save trend data to the file
-    with open(os.path.join(results_dir, file_name), 'w') as trend_file:
+    with open(os.path.join(results_dir, file_name), "w") as trend_file:
         json.dump(trend, trend_file, sort_keys=True)
 
 
-def load_trend(results_dir, file_name='trend.json'):
+def load_trend(results_dir, file_name="trend.json"):
     # Return list of summaries loaded from tests trend file
     # If file is broken, empty or not found create new trend list
     try:
-        with open(os.path.join(results_dir, file_name), 'r') as trend_file:
+        with open(os.path.join(results_dir, file_name), "r") as trend_file:
             trend = json.load(trend_file)
     except (IOError, json.decoder.JSONDecodeError):
         trend = []
@@ -94,9 +96,15 @@ def update_trend(summary, trend):
     # Append result summary if trend has less than two results or
     # the last one result is different than current,
     # otherwise replace last summary
-    if len(trend) < 2 or len(summary.keys()) != len(trend[-1].keys()) or \
-       any(trend[-1].get(key) != summary.get(key)
-           for key in summary.keys() if key != 'date'):
+    if (
+        len(trend) < 2
+        or len(summary.keys()) != len(trend[-1].keys())
+        or any(
+            trend[-1].get(key) != summary.get(key)
+            for key in summary.keys()
+            if key != "date"
+        )
+    ):
         trend.append(summary)
     else:
         trend[-1] = summary
--- test/test_backend.py
+++ test/test_backend.py
@@ -9,8 +9,8 @@ import onnx.backend.test
 # Custom backend importer
 def import_backend(onnx_backend_module):
     backend = importlib.import_module(onnx_backend_module)
-    if not hasattr(backend, 'run_model') and not hasattr(backend, 'run'):
-        raise ValueError('%s is not a valid ONNX backend', onnx_backend_module)
+    if not hasattr(backend, "run_model") and not hasattr(backend, "run"):
+        raise ValueError("%s is not a valid ONNX backend", onnx_backend_module)
     return backend
 
 
@@ -18,16 +18,16 @@ def import_backend(onnx_backend_module):
 backend = import_backend(test.ONNX_BACKEND_MODULE)
 
 # Set backend device name to be used
-backend.backend_name = 'CPU'
+backend.backend_name = "CPU"
 
 # This is a pytest variable to load extra plugins
 # Enable the ONNX compatibility report
-pytest_plugins = 'onnx.backend.test.report'
+pytest_plugins = "onnx.backend.test.report"
 
 # Import all test cases at global scope to make them visible to python.unittest
 backend_test = onnx.backend.test.BackendTest(backend, __name__)
 globals().update(backend_test.enable_report().test_cases)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
