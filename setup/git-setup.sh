06af418bac9687c82a27796ce51a4d76443802f6
Katarzyna Mitrus <katarzyna.mitrus@intel.com>
2019-11-08 12:21:29 +0100

    Apply suggestions from code review
    Review suggestions
    
    Co-Authored-By: Michał Karzyński <postrational@users.noreply.github.com>

--- setup/git-setup.sh
+++ setup/git-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
-git config --global user.email "scoreboard@pipeline.com"
-git config --global user.name "scoreboard-pipeline"
+git config --global user.email "onnx_scoreboard_bot@azure"
+git config --global user.name "ONNX Scoreboard Bot @ Azure Pipelines"
 
 git pull origin master
 git checkout master
