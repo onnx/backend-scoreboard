b1e934875522a6cbb522e1d8624f853b502066c0
mitruska <katarzyna.mitrus@intel.com>
2019-11-12 16:47:25 +0100

    Update git-deploy-website.sh

--- setup/git-deploy-website.sh
+++ setup/git-deploy-website.sh
@@ -3,9 +3,9 @@
 set -e  # Exit on error
 set -x  # Command echo on
 
-git pull origin master
+git remote set-url origin git@github.com:onnx/backend-scoreboard.git
+git checkout master
+git pull
 git add docs
-git commit --allow-empty -m "Upadate website [skip ci]"
-git remote set-url --push origin git@github.com:onnx/backend-scoreboard.git
-git pull origin master
-git push origin HEAD:master
+git commit -m "Upadate website [skip ci]" || true  # Not failed if nothing to commit
+git push
