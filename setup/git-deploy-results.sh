07da7dd7bd64063d623bd6a8cc6717a7deb2795c
Katarzyna Mitrus <katarzyna.mitrus@intel.com>
2019-11-15 13:48:13 +0100

    Update git-deploy scripts (#13)

--- setup/git-deploy-results.sh
+++ setup/git-deploy-results.sh
@@ -3,9 +3,9 @@
 set -e  # Exit on error
 set -x  # Command echo on
 
-git pull origin master
+git remote set-url origin git@github.com:onnx/backend-scoreboard.git
+git checkout master
+git pull
 git add results
 git commit -m "Scoreboard results [skip ci]"
-git remote set-url --push origin git@github.com:onnx/backend-scoreboard.git
-git pull origin master
-git push origin HEAD:master
+git push
