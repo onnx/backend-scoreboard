effc2a4e9f3706f7c6f175ee229a0422591df2b7
mitruska <katarzyna.mitrus@intel.com>
2019-11-16 22:21:44 +0100

    Moved git remote url setting from deploy to setup script

--- setup/git-deploy-website.sh
+++ setup/git-deploy-website.sh
@@ -3,7 +3,6 @@
 set -e  # Exit on error
 set -x  # Command echo on
 
-git remote set-url origin git@github.com:onnx/backend-scoreboard.git
 git checkout master
 git pull
 git add docs
--- setup/git-setup.sh
+++ setup/git-setup.sh
@@ -4,6 +4,7 @@ set -e  # Exit on error
 set -x  # Command echo on
 
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+git remote set-url origin git@github.com:onnx/backend-scoreboard.git
 
 git config --global user.email "onnx_scoreboard_bot@azure"
 git config --global user.name "ONNX Scoreboard Bot @ Azure Pipelines"
