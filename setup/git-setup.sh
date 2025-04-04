ae3238f3090b72182025d363c7ca69c6c63f282d
Michał Karzyński <michal.karzynski@intel.com>
2025-04-04 12:50:51 +0200

    Transition to GitHub actions (#62)
    * Transition to GitHub actions
    
    Signed-off-by: Michal Karzynski <michal.karzynski@intel.com>
    
    * Bugfix
    
    Signed-off-by: Michal Karzynski <michal.karzynski@intel.com>
    
    * Update .github/workflows/benchmark-backends.yml
    
    Co-authored-by: Katarzyna Mitrus <katarzyna.mitrus@intel.com>
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    ---------
    
    Signed-off-by: Michal Karzynski <michal.karzynski@intel.com>
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    Co-authored-by: Andreas Fehlner <fehlner@arcor.de>
    Co-authored-by: Katarzyna Mitrus <katarzyna.mitrus@intel.com>

--- setup/git-setup.sh
+++ setup/git-setup.sh
@@ -6,11 +6,10 @@
 set -e  # Exit on error
 set -x  # Command echo on
 
-ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 git remote set-url origin git@github.com:onnx/backend-scoreboard.git
 
-git config --global user.email "onnx_scoreboard_bot@azure"
-git config --global user.name "ONNX Scoreboard Bot @ Azure Pipelines"
+git config --global user.name "GitHub Action"
+git config --global user.email "action@github.com"
 
-git pull origin master
+git fetch origin master
 git checkout master
