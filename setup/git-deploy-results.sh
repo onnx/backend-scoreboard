891280bb68b84ca5a76577a34780ac557331f486
Andreas Fehlner <fehlner@arcor.de>
2026-03-10 17:39:37 +0100

    maintenance (#85)
    * update
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    * update
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>
    
    ---------
    
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- setup/git-deploy-results.sh
+++ setup/git-deploy-results.sh
@@ -6,12 +6,20 @@
 set -e  # Exit on error
 set -x  # Command echo on
 
-git checkout master
-git pull
+git fetch origin
+
+# Switch to benchmark-results branch or create it as orphan on first run
+if git ls-remote --heads origin benchmark-results | grep -q benchmark-results; then
+  git checkout -B benchmark-results origin/benchmark-results
+else
+  git checkout --orphan benchmark-results
+  git rm -rf --cached . 2>/dev/null || true
+fi
+
 git add results
 if ! git diff --cached --quiet; then
   git commit -s -m "Scoreboard results [skip ci]"
-  git push
+  git push origin benchmark-results
 else
   echo "No changes to commit"
 fi
