ffed526b8044bd7c47c18ed5637b0ca2e3a19ca8
Andreas Fehlner <fehlner@arcor.de>
2026-03-10 17:39:08 +0100

    update
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- setup/git-setup.sh
+++ setup/git-setup.sh
@@ -6,8 +6,6 @@
 set -e  # Exit on error
 set -x  # Command echo on
 
-git remote set-url origin git@github.com:onnx/backend-scoreboard.git
-
 git config --global user.name "GitHub Action"
 git config --global user.email "action@github.com"
 
